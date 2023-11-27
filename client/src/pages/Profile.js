5bab9368eb695da2a8dedebeb1d113fe34c9ed0c
Reilly <reillyswork@gmail.com>
2023-11-27 12:28:26 -0700

    Update client/src/pages/Profile.js on main

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import {useHistory, Link } from "react-router-dom";
+import Card from "./Card";
+
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const history = useHistory();
+  const [user, setUser] = useState(null);
+  const [savedCrafts, setSavedCrafts] = useState([]);
 
-export default Profile
+  // Placeholder data
+  const initialSavedCrafts = [
+    { id: 1, title: "Craft 1" },
+    { id: 2, title: "Craft 2" },
+  ];
+
+  useEffect(() => {
+    // Fetch user profile data 
+    fetchProfile(); 
+  }, []);
+
+  // function for fetching user profile
+  const fetchProfile = () => {
+    // logic need to go here
+    // setUser(result); // Set the user state with the fetched data
+    setUser({
+      name: "Ribbit Robbit",
+      username: "frog_queen",
+      bio: "Bow Down To The Frog Queen!",
+    });
+    setSavedCrafts(initialSavedCrafts);
+  };
+
+  // functions for saving, editing, and deleting crafts
+  const saveCraft = (craft) => {
+    // logic!
+    setSavedCrafts((prevCrafts) => [...prevCrafts, craft]);
+  };
+
+  const editProfile = () => {
+    // logic!
+  };
+
+  const deleteProfile = () => {
+    // logic
+    history.push("/"); // to go back home
+  };
+
+  const removeSavedCraft = (craftId) => {
+    // logic
+    setSavedCrafts((prevCrafts) =>
+      prevCrafts.filter((craft) => craft.id !== craftId)
+    );
+  };
+
+  return (
+    <div>
+      {user && (
+        <>
+          <h2>{user.name}'s Profile</h2>
+          <p>Username: {user.username}</p>
+          <p>Bio: {user.bio}</p>
+          {/* logic */}
+          <Link to="/edit-profile">Edit Profile</Link>
+          <button onClick={deleteProfile}>Delete Profile</button>
+
+          <h3>Saved Crafts</h3>
+          {savedCrafts.map((craft) => (
+            <CraftCard
+              key={craft.id}
+              craft={craft}
+              onSave={() => saveCraft(craft)}
+              onRemove={() => removeSavedCraft(craft.id)}
+              editable
+            />
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default UserProfile;
