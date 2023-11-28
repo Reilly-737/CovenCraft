bff3f965e39fd538d39cdc75885d6dd59e35f766
Kat Tannehill <katetannehill@gmail.com>
2023-11-28 10:00:18 -0700

    Basic Alert Bar code

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import AlertBar from "./components/AlertBar";
 
 const App = () => {
+    const [message, setMessage] = useState(null);
+    const [snackType, setSnackType] = useState("");
+
+    const setAlertMessage = (msg) => setMessage(msg);
+
+    const handleSnackType = (type) => setSnackType(type);
 
     return (
         <div>
             <Header />
+            {message && (
+                <AlertBar
+                    message={message}
+                    snackType={snackType}
+                    setAlertMessage={setAlertMessage}
+                    handleSnackType={handleSnackType}
+                />
+            )}
             <div id="outlet">
                 <Outlet />
             </div>
--- client/src/components/AlertBar.js
+++ client/src/components/AlertBar.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useState, forwardRef } from "react";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
 
-const AlertBar = () => {
+const Alert = forwardRef((props, ref) => (
+  <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
+));
+
+const AlertBar = ({ message, setAlertMessage, snackType, handleSnackType }) => {
+  const [open, setOpen] = useState(true);
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+    setAlertMessage(null);
+    handleSnackType("");
+  };
+
   return (
-    <div>AlertBar</div>
-  )
-}
+    <Snackbar open={open} autoHideDuration={5000} onClose={handleClose}>
+      <Alert onClose={handleClose} severity={snackType} sx={{ width: "100%" }}>
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+};
 
-export default AlertBar
+export default AlertBar;
