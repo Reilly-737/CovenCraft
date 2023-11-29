ff53832f8c315e1383db1f173817638936ed18f1
Kat Tannehill <katetannehill@gmail.com>
2023-11-28 19:56:19 -0700

    Add save/ Remove craft button and logic to View One

--- client/src/pages/ViewOne.js
+++ client/src/pages/ViewOne.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { useOutletContext, useParams } from 'react-router-dom'
 
 const ViewOne = () => {
-  const { setAlertMessage, handleSnackType } = useOutletContext()
+  const { user, setAlertMessage, handleSnackType } = useOutletContext()
   const { id } = useParams()
   const [craft, setCraft] = useState({})
   const { image, title, description, instructions, difficulty, materials, witches } = craft
@@ -16,6 +16,40 @@ const ViewOne = () => {
         setAlertMessage(errorObj.message);
     })
   }, [id])
+
+  const handleSaveCraft = () =>{
+    fetch('/witch_crafts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ craft_id: id }),
+    })
+      .then((resp) => resp.json())
+      .then(data => {
+        handleSnackType("success");
+        setAlertMessage("Craft added!");
+      })
+      .catch(errorObj => {
+        handleSnackType("error");
+        setAlertMessage(errorObj.message);
+      })
+  }
+
+  const handleDeleteCraft = (id) =>{
+    fetch(`/witch_crafts/${id}`, {
+      method: 'DELETE',
+    })
+      .then((resp) => resp.json())
+      .then(data => {
+        handleSnackType("success");
+        setAlertMessage("Craft removed!");
+      })
+      .catch(errorObj => {
+        handleSnackType("error");
+        setAlertMessage(errorObj.message);
+      })
+  }
 
   const materials_list = materials?.map(material => <li key={material["name"]}>{material["name"]}</li>)
   const witch_list = witches?.map(witch => <li key={witch["username"]}>{witch["username"]}</li>)
@@ -31,7 +65,10 @@ const ViewOne = () => {
           <p>{description}</p>
           <p>{instructions}</p>
           <ul>{materials_list}</ul>
-          <button>Save craft</button>
+          { user ? (
+            <button onClick={() => handleDeleteCraft(id)}>Remove craft</button> ) : (
+            <button onClick={handleSaveCraft}>Save craft</button>
+          )}
         </main>
         <aside>
           <h3>Coven Crafters:</h3>
