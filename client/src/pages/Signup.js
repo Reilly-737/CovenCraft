5e5468191aae9fee4d06f2fada161d49d9864c7f
Jazlin Yu <jazlinsub@gmail.com>
2023-11-29 16:41:34 -0800

    auth fixed with post method

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,92 +1,116 @@
-import React from "react";
-
+import React, { useState } from "react";
+// import { useHistory } from "react-router-dom";
+// import styled from "styled-components";
 import { useFormik } from "formik";
-import * as Yup from "yup";
+import * as yup from "yup";
 
-export default function App() {
+function SignUp({ updateUser, handleNewError }) {
+  const [signUp, setSignUp] = useState(true); // Set to true for sign-up only
+  // const history = useHistory();
+  // const { user, setAlertMessage, handleSnackType } = useOutletContext();
+  const handleClick = () => setSignUp(true); // Always set to true for sign-up
+  const signupSchema = yup.object().shape({
+    username: yup.string().required("Please enter a witch name"),
+    email: yup
+      .string()
+      .email("Must be a valid email")
+      .required("Please enter an email"),
+    password: yup
+      .string()
+      .required("Please enter a witchy password")
+      .min(8, "Password is too short - should be 8 chars minimum.")
+      .matches(/[a-zA-Z0-9]/, "Password can only contain letters and numbers."),
+  });
+  const url = signUp ? "/signup" : "/login";
+
   const formik = useFormik({
     initialValues: {
-      witch_name: "",
+      username: "",
       email: "",
       password: "",
-      confirm_password: "",
     },
-    validationSchema: Yup.object({
-      witch_name_name: Yup.string()
-        .min(2, "Mininum 2 characters")
-        .max(15, "Maximum 15 characters")
-        .required("Required!"),
-      email: Yup.string().email("Invalid email format").required("Required!"),
-      password: Yup.string()
-        .min(8, "Minimum 8 characters")
-        .required("Required!"),
-      confirm_password: Yup.string()
-        .oneOf([Yup.ref("password")], "Password's not match")
-        .required("Required!"),
-    }),
+    validationSchema: signUp ? signupSchema : null,
     onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      })
+        .then((res) => {
+          if (res.ok) {
+            res.json().then(updateUser);
+          } else {
+            res.json().then((errorObj) => handleNewError(errorObj.message));
+          }
+        })
+        .catch(handleNewError);
     },
   });
 
   return (
-    <div className="App">
-      <h1>Join The Coven</h1>
-
-      <form onSubmit={formik.handleSubmit}>
-        <div>
-          <label>Witch Name</label>
-          <input
-            type="text"
-            name="witch_name"
-            value={formik.values.witch_name}
-            onChange={formik.handleChange}
-          />
-          {formik.errors.witch_name && formik.touched.witch_name && (
-            <p>{formik.errors.witch_name}</p>
-          )}
-        </div>
-        <div>
-          <label>Email</label>
-          <input
-            type="email"
-            name="email"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-          />
-          {formik.errors.email && formik.touched.email && (
-            <p>{formik.errors.email}</p>
-          )}
-        </div>
-        <div>
-          <label>Password</label>
-          <input
-            type="password"
-            name="password"
-            value={formik.values.password}
-            onChange={formik.handleChange}
-          />
-          {formik.errors.password && formik.touched.password && (
-            <p>{formik.errors.password}</p>
-          )}
-        </div>
-        <div>
-          <label>Confirm Password</label>
-          <input
-            type="password"
-            name="confirm_password"
-            value={formik.values.confirm_password}
-            onChange={formik.handleChange}
-          />
-          {formik.errors.confirm_password &&
-            formik.touched.confirm_password && (
-              <p>{formik.errors.confirm_password}</p>
-            )}
-        </div>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
+    <>
+      <div>
+        <h2>Join The Coven!</h2>
+      </div>
+      <Form onSubmit={formik.handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          name="email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+        />
+        {formik.errors.email ? (
+          <div className="error-message show">{formik.errors.email}</div>
+        ) : null}
+        {signUp && (
+          <>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              value={formik.values.username}
+              onChange={formik.handleChange}
+            />
+            {formik.errors.username ? (
+              <div className="error-message show">{formik.errors.username}</div>
+            ) : null}
+          </>
+        )}
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+        />
+        {formik.errors.password ? (
+          <div className="error-message show">{formik.errors.password}</div>
+        ) : null}
+        <input type="submit" value="Sign Up!" />
+      </Form>
+    </>
   );
 }
+
+export default SignUp;
+
+// export const Form = styled.form`
+//   display: flex;
+//   flex-direction: column;
+//   width: 400px;
+//   margin: auto;
+//   font-family: Arial;
+//   font-size: 30px;
+//   input[type="submit"] {
+//     background-color: #42ddf5;
+//     color: white;
+//     height: 40px;
+//     font-family: Arial;
+//     font-size: 30px;
+//     margin-top: 10px;
+//     margin-bottom: 10px;
+//   }
+// `;
