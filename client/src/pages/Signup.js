a86db1d899acacd3f490a071f2de381eb42ea789
Jazlin Yu <jazlinsub@gmail.com>
2023-11-30 12:15:36 -0800

    signup edits

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,116 +1,126 @@
 import React, { useState } from "react";
-// import { useHistory } from "react-router-dom";
-import styled from "styled-components";
 import { useFormik } from "formik";
 import * as yup from "yup";
+import { useOutletContext } from "react-router-dom";
 
-function SignUp({ updateUser, handleNewError }) {
-  const [signUp, setSignUp] = useState(true); // Set to true for sign-up only
-  // const history = useHistory();
-  // const { user, setAlertMessage, handleSnackType } = useOutletContext();
-  const handleClick = () => setSignUp(true); // Always set to true for sign-up
-  const signupSchema = yup.object().shape({
-    username: yup.string().required("Please enter a witch name"),
-    email: yup
-      .string()
-      .email("Must be a valid email")
-      .required("Please enter an email"),
-    password: yup
-      .string()
-      .required("Please enter a witchy password")
-      .min(8, "Password is too short - should be 8 chars minimum.")
-      .matches(/[a-zA-Z0-9]/, "Password can only contain letters and numbers."),
+const Signup = () => {
+  const { setAlertMessage, handleSnackType } = useOutletContext();
+  const [snackbar, setSnackbar] = useState({
+    // open: false,
+    // message: "",
+    // type: "success",
   });
-  const url = signUp ? "/signup" : "/login";
 
   const formik = useFormik({
     initialValues: {
+      // Define your form fields here
       username: "",
       email: "",
       password: "",
+      confirmPassword: "",
     },
-    validationSchema: signUp ? signupSchema : null,
-    onSubmit: (values) => {
-      fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(values),
-      })
-        .then((res) => {
-          if (res.ok) {
-            res.json().then(updateUser);
-          } else {
-            res.json().then((errorObj) => handleNewError(errorObj.message));
-          }
-        })
-        .catch(handleNewError);
+    validationSchema: yup.object({
+      // Define validation rules using yup
+      username: yup.string().required("Username is required"),
+      email: yup
+        .string()
+        .email("Invalid email address")
+        .required("Email is required"),
+      password: yup
+        .string()
+        .required("Password is required")
+        .min(6, "Password must be at least 6 characters"),
+      confirmPassword: yup
+        .string()
+        .oneOf([yup.ref("password"), null], "Passwords must match"),
+    }),
+    onSubmit: async (values) => {
+      try {
+        // Make a POST request to your Flask API endpoint
+        const response = await fetch("/witch", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(values),
+        });
+
+        // Check if the response is successful (you may need to adjust this based on your actual API response)
+        if (response.ok) {
+          setAlertMessage("Welcome to the Coven!");
+          handleSnackType("success");
+        } else {
+          const data = await response.json();
+          setAlertMessage(data.error || "An error occurred during signup.");
+          handleSnackType("error");
+        }
+      } catch (error) {
+        // Handle network errors or other exceptions
+        setAlertMessage("An error occurred during signup.");
+        handleSnackType("error");
+      }
     },
   });
 
   return (
-    <>
-      <div>
-        <h2>Join The Coven!</h2>
-      </div>
-      <Form onSubmit={formik.handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <input
-          type="text"
-          name="email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-        />
-        {formik.errors.email ? (
-          <div className="error-message show">{formik.errors.email}</div>
-        ) : null}
-        {signUp && (
-          <>
-            <label htmlFor="username">Username</label>
-            <input
-              type="text"
-              name="username"
-              value={formik.values.username}
-              onChange={formik.handleChange}
-            />
-            {formik.errors.username ? (
-              <div className="error-message show">{formik.errors.username}</div>
-            ) : null}
-          </>
-        )}
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-        />
-        {formik.errors.password ? (
-          <div className="error-message show">{formik.errors.password}</div>
-        ) : null}
-        <input type="submit" value="Sign Up!" />
-      </Form>
-    </>
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={formik.handleSubmit}>
+        {/* Username Field */}
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.username}
+          />
+          {formik.touched.username && formik.errors.username && (
+            <div className="error">{formik.errors.username}</div>
+          )}
+        </div>
+
+        {/* Email Field */}
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email && (
+            <div className="error">{formik.errors.email}</div>
+          )}
+        </div>
+
+        {/* Password Field */}
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+          />
+          {formik.touched.password && formik.errors.password && (
+            <div className="error">{formik.errors.password}</div>
+          )}
+        </div>
+
+        {/* Submit Button */}
+        <div>
+          <button type="submit">Sign Up</button>
+        </div>
+      </form>
+    </div>
   );
-}
+};
 
-export default SignUp;
-
-export const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  width: 400px;
-  margin: auto;
-  font-family: Arial;
-  font-size: 30px;
-  input[type="submit"] {
-    background-color: #42ddf5;
-    color: white;
-    height: 40px;
-    font-family: Arial;
-    font-size: 30px;
-    margin-top: 10px;
-    margin-bottom: 10px;
-  }
-`;
+export default Signup;
