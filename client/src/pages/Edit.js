a2d9e7cc16689b753fb9214fa3082691346ba50c
Kat Tannehill <katetannehill@gmail.com>
2023-11-30 21:24:01 -0700

    Fixed sign in I think

--- client/src/pages/Edit.js
+++ client/src/pages/Edit.js
@@ -3,35 +3,14 @@ import { useOutletContext } from "react-router-dom";
 import FormComp from "../components/Form";
 
 const Edit = () => {
-  const { user, setAlertMessage, handleSnackType } = useOutletContext()
-  const [userInfo, setUserInfo] = useState({})
 
-  useEffect(() => {
-    if (user) {
-      fetch(`/witches/${user.id}`)
-      .then(resp => {
-        if (resp.ok) {
-          resp.json().then(setUserInfo)
-        } else {
-          resp.json().then(err => {
-            handleSnackType("error")
-            setAlertMessage(err.message)
-          })
-        }
-      })
-      .catch(err => {
-        handleSnackType("error")
-        setAlertMessage(err.message)
-      })
-    }
-  }, [user])
 
   return (
 
     <div className="main">
       <h2>Edit Profile</h2>
       <div>
-        <FormComp userInfo={userInfo} />
+        <FormComp />
       </div>
     </div>
   )
